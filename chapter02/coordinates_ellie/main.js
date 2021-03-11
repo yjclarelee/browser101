6b5b02c624a9388f422630c8148e781a62de7d7d
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-03-11 14:46:29 +0900

    Changed setAttribute to style.attribute

--- chapter02/coordinates_ellie/main.js
+++ chapter02/coordinates_ellie/main.js
@@ -6,13 +6,13 @@ const span = document.querySelector('span');
 window.addEventListener('mousemove', (event) => {
     const x = event.clientX;
     const y = event.clientY;
-    horizontal.setAttribute('style', `top:${y}px;`);  
-    vertical.setAttribute('style', `left:${x}px;`);
-
-    img.setAttribute('style', `left:${x}px; top:${y}px;`);
-
+    horizontal.style.top = `${y}px`;
+    vertical.style.left = `${x}px`;
+    img.style.left = `${x}px`;
+    img.style.top = `${y}px`;
     span.innerHTML = `${x}px, ${y}px`;
-    span.setAttribute('style', `left:${x}px; top:${y}px;`);
+    span.style.left = `${x}px`;
+    span.style.top = `${y}px`;
 })
 
 
