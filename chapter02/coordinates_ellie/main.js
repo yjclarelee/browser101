1431298933bbf35c4abf588d21aac495a1f6faad
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-03-12 07:20:28 +0900

    Change set style to browser friendly transform

--- chapter02/coordinates_ellie/main.js
+++ chapter02/coordinates_ellie/main.js
@@ -6,13 +6,22 @@ const span = document.querySelector('span');
 window.addEventListener('mousemove', (event) => {
     const x = event.clientX;
     const y = event.clientY;
-    horizontal.style.top = `${y}px`;
-    vertical.style.left = `${x}px`;
-    img.style.left = `${x}px`;
-    img.style.top = `${y}px`;
+    // top and left cause layout
+    // horizontal.style.top = `${y}px`;
+    // vertical.style.left = `${x}px`;
+    // img.style.left = `${x}px`;
+    // img.style.top = `${y}px`;
+    // span.innerHTML = `${x}px, ${y}px`;
+    // span.style.left = `${x}px`;
+    // span.style.top = `${y}px`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    vertical.style.transform = `translateX(${x}px)`;
+
+    let imgLen = img.clientWidth;
+    img.style.transform = `translate(${x-imgLen/2}px, ${y-imgLen/2}px)`;
     span.innerHTML = `${x}px, ${y}px`;
-    span.style.left = `${x}px`;
-    span.style.top = `${y}px`;
+    span.style.transform = `translate(${x+20}px, ${y+20}px)`;
+    
 })
 
 
