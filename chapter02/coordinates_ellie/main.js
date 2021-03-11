2dd9280d66d6e0676d3969df0b5fa73c203220aa
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-03-12 07:51:40 +0900

    Add comments

--- chapter02/coordinates_ellie/main.js
+++ chapter02/coordinates_ellie/main.js
@@ -6,14 +6,10 @@ const span = document.querySelector('span');
 window.addEventListener('mousemove', (event) => {
     const x = event.clientX;
     const y = event.clientY;
-    // top and left cause layout
+    // This kind of 'top' refering code is not used because it causes a lot of layouts to be made(bad efficiency)
     // horizontal.style.top = `${y}px`;
-    // vertical.style.left = `${x}px`;
-    // img.style.left = `${x}px`;
-    // img.style.top = `${y}px`;
-    // span.innerHTML = `${x}px, ${y}px`;
-    // span.style.left = `${x}px`;
-    // span.style.top = `${y}px`;
+
+    // instead, transform is used
     horizontal.style.transform = `translateY(${y}px)`;
     vertical.style.transform = `translateX(${x}px)`;
 
