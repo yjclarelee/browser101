bb6a7f832edcc3539f7592b3c462b729d7b36506
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-03-11 10:57:18 +0900

    Set actions according to button click

--- chapter01/x_y_coordinate/app.js
+++ chapter01/x_y_coordinate/app.js
@@ -1,3 +1,8 @@
+function render(){
+    getCoordinatesOnClick();
+    onButtonClick();
+}
+
 function getCoordinatesOnClick(){
     const rect = document.querySelector('.rects');
     rect.addEventListener('click', (event) => {
@@ -7,4 +12,22 @@ function getCoordinatesOnClick(){
     })
 }
 
-getCoordinatesOnClick();
+function onButtonClick(){
+    const buttons = document.querySelector('.buttons');
+    const special = document.querySelector('.special')
+    buttons.addEventListener('click', (event) => {
+        console.log(event.target);
+        const className = event.target.className;
+        if(className == 'by'){
+            window.scrollBy(0, 100);
+        }
+        else if(className == 'to'){
+            window.scrollTo(0, 100);
+        }
+        else{
+            special.scrollIntoView();
+        }
+    })
+}
+
+render();
