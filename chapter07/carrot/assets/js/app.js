535c1ad694eb2c5860d298efa8c2c845d9c9242d
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-03-12 19:13:52 +0900

    Add items randomly

--- chapter07/carrot/assets/js/app.js
+++ chapter07/carrot/assets/js/app.js
@@ -1,18 +1,23 @@
-function render(){
+const GAME_TIME = 3;
+const TOP_LENGTH = 220;
+const X_AVAILABLE_LENGTH = 700;
+const Y_AVAILABLE_LENGTH = 130;
+
+(function render(){
     startGame();
-}
+})();
 
 function startGame(){
     const playButton = document.querySelector('.play');
     playButton.addEventListener('click', (event) => {
         setTimer(playButton);
-        // setItems();
+        setItems();
     })
 }
 
 function setTimer(){
     const timeSpan = document.querySelector('.time');
-    let time = 3;
+    let time = GAME_TIME;
     const interval = setInterval(() => {
         timeSpan.innerHTML = `00:${padZero(time-1)}`;
         time--;
@@ -20,7 +25,7 @@ function setTimer(){
             console.log('finished');
             clearInterval(interval); 
             // endGame()
-    }
+        }
     }, 1000);
 }
 
@@ -29,4 +34,25 @@ function padZero(num){
     else return '0' + num;
 }
 
-render();
+function setItems(){
+    setItem('carrot');
+    setItem('bug');
+}
+
+function setItem(item){
+    const list = document.querySelector(`.${item}s`);
+    for(let i = 0; i < 10; i++){
+        let x = Math.random() * X_AVAILABLE_LENGTH;
+        let y = Math.random() * Y_AVAILABLE_LENGTH;
+        const li = makeHTML(x, y, i, item);
+        list.appendChild(li);
+    }
+}
+
+function makeHTML(x, y, id, type){
+    const li = document.createElement('li');
+    li.setAttribute('data-id', `${type}-${id}`);
+    li.innerHTML = `<img src="./assets/img/${type}.png" alt="${type}">`;
+    li.style.transform = `translate(${x}px, ${y}px)`
+    return li;   
+}
